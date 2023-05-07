91de4d7f53a7e5ba10af202398047cb5ff96e5dc
hemavmane <hemamane5376@gmail.com>
2023-05-07 22:06:29 +0530

    7-may

---
+++ Methods.js
@@ -0,0 +1,48 @@
+// concat()
+
+// let arr = [4, 3,34,3,34,65,2,19]
+// let arr2 = [4,4,3,83,30,9,2,5]
+// let str1= ['hello','world']
+// let str2 = ['lets','start','code']
+
+
+// // let arr3 = arr.concat(arr2)
+// // console.log(arr3)
+
+// let str3 = str1.concat(str2)
+// console.log(str3)
+
+// // entries
+// let arr = [4, 3,34,3,34,65,2,19]
+// let arr2 = [4,4,3,83,30,9,2,5]
+// let str1= ['hello','world']
+// let str2 = ['lets','start','code']
+
+
+// let k= arr.entries().next().value
+// console.log(k)
+
+
+// let arr = [4, 3,34,6,34,65,2,19]
+// let arr2 = [4,4,3,83,30,9,2,5]
+// let str1= ['hello','world']
+// let str2 = ['lets','start','code']
+
+// copyWithin(target)
+// copyWithin(target, start)
+// copyWithin(target, start, end)
+
+// arr.copyWithin(0,4)
+// console.log(arr)
+
+//return  the modified array.it copy the element to index which is selected by us
+
+
+
+let arr = [4, 3,34,6,34,65,2,19]
+let arr2 = [4,4,3,83,30,9,2,5]
+let str1= ['hello','world']
+let str2 = ['lets','start','code']
+// arr.every() it will see each condition  in case of some() it will if one conditon satisfied measn
+// it sreturn true 
+
